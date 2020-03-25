17cd6658510abee5d6b1e107d7a16823b0b7059a
Sanjeev Thapa <sam.gaha@icloud.com>
2020-03-25 09:33:51 +0545

    My first commit

---
+++ First_WebApp/views.py
@@ -0,0 +1,16 @@
+import operator
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from First_WebApp import counter
+
+
+def home(request):
+
+    return render(request,'index.html',{'key1':'I am coming from python code'})
+
+def result(request):
+    article =request.GET['article']
+    var_dict,word_count = counter.count(article)
+    return render(request,'result.html',{'article':article,'word_count':word_count,'dict_words':var_dict})
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\sanjeev\PycharmProjects\First_WebApp\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
